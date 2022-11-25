6197f48a819d6b3091f7b4430bf636ed7e222b8d
Alesso-Chiavarino <alessochiavarino@gmail.com>
2022-11-25 15:37:57 -0300

    update

--- src/components/WhatsAppIcon/WhatsAppIcon.jsx
+++ src/components/WhatsAppIcon/WhatsAppIcon.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa'
 const WhatsAppIcon = () => {
   return (
     <div className="icono_whastapp">
-      <a href="#" rel="noreferrer" target="_blank"><FaWhatsapp className='whatsAppIcon' /></a>
+      <a href="https://wa.me/3513079987" rel="noreferrer" target="_blank"><FaWhatsapp className='whatsAppIcon' /></a>
     </div>
   )
 }
